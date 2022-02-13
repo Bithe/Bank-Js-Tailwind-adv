0c6d4aff98f20305cfce6867e60bb1d949fa15c7
bithe <farzana.hossain5@gmail.com>
2022-02-13 16:15:41 +0600

    deposit done

--- js/deposit_withdraw.js
+++ js/deposit_withdraw.js
@@ -1,1 +1,11 @@
-document
+document.getElementById('deposit-submit').addEventListener("click", function() {
+    const depositAmount = document.getElementById('deposit-amount');
+    const deposit = depositAmount.value;
+
+    //get current deposit
+    const totalDepositAmount = document.getElementById('total-deposit-amount');
+
+    totalDepositAmount.innerText = deposit;
+    depositAmount.value = "";
+
+});
