ea8cc47f125e814a425a91e5cc229737234668be
bithe <farzana.hossain5@gmail.com>
2022-02-13 18:27:22 +0600

    function for deposit and withdraw shortcut

--- js/deposit_withdraw.js
+++ js/deposit_withdraw.js
@@ -1,17 +1,25 @@
-document.getElementById('deposit-submit').addEventListener("click", function() {
-
-    const totalAmount = document.getElementById('deposit-amount');
-    const depositText = totalAmount.value;
+function getInputValue(inputId) {
+    const inputField = document.getElementById(inputId);
+    const depositText = inputField.value;
     const deposit = parseFloat(depositText);
-    //
+    inputField.value = '';
+    return deposit;
+}
+//
+document.getElementById('deposit-submit').addEventListener("click", function() {
+    const deposit = getInputValue('deposit-amount');
+    /*const totalAmount = document.getElementById('deposit-amount');
+    const depositText = totalAmount.value;
+    const    = parseFloat(depositText);
+
+     totalAmount.value = '';
+    */
     const totalDepositAmount = document.getElementById('total-deposit-amount');
     const totalDepositAmountText = totalDepositAmount.innerText;
 
     totalDepositAmount.innerText = parseFloat(totalDepositAmountText) + deposit;
 
-    totalAmount.value = '';
-
-    //Update 
+    //Update Balance
     const balance = document.getElementById('balance');
     const balanceText = balance.innerText;
     balance.innerText = parseFloat(balanceText) + deposit;
@@ -19,17 +27,17 @@ document.getElementById('deposit-submit').addEventListener("click", function() {
 
 //Withdraw function
 document.getElementById('withdraw-submit').addEventListener("click", function() {
-    const withdrawAmount = document.getElementById('withdraw-amount');
+    /*const withdrawAmount = document.getElementById('withdraw-amount');
     const withdrawText = withdrawAmount.value;
     const withdraw = parseFloat(withdrawText);
+    //Clear the withdraw input field
+    withdrawAmount.value = ""; */
 
+    const withdraw = getInputValue('withdraw-amount');
     //
     const totaWithdrawAmount = document.getElementById('total-withdraw-amount');
     const totalWithdrawText = totaWithdrawAmount.innerText;
     totaWithdrawAmount.innerText = parseFloat(totalWithdrawText) + withdraw;
-
-    //Clear the withdraw input field
-    withdrawAmount.value = "";
 
     //Update balance with withdraw
     const balance = document.getElementById('balance');
@@ -37,6 +45,6 @@ document.getElementById('withdraw-submit').addEventListener("click", function() 
     const previousBalanceTotal = parseFloat(balanceText);
     balance.innerText =
         previousBalanceTotal - withdraw;
-    //Clear the withdraw input field
-    withdrawAmount.value = "";
+    /*//Clear the withdraw input field
+    withdrawAmount.value = ""; */
 });
