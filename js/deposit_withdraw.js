ab721e17f988db6c32a3c812a226e70f13b3fb65
bithe <farzana.hossain5@gmail.com>
2022-02-13 17:44:49 +0600

    withd and blnc done

--- js/deposit_withdraw.js
+++ js/deposit_withdraw.js
@@ -1,11 +1,42 @@
 document.getElementById('deposit-submit').addEventListener("click", function() {
-    const depositAmount = document.getElementById('deposit-amount');
-    const deposit = depositAmount.value;
 
-    //get current deposit
+    const totalAmount = document.getElementById('deposit-amount');
+    const depositText = totalAmount.value;
+    const deposit = parseFloat(depositText);
+    //
     const totalDepositAmount = document.getElementById('total-deposit-amount');
+    const totalDepositAmountText = totalDepositAmount.innerText;
 
-    totalDepositAmount.innerText = deposit;
-    depositAmount.value = "";
+    totalDepositAmount.innerText = parseFloat(totalDepositAmountText) + deposit;
 
+    totalAmount.value = '';
+
+    //Update 
+    const balance = document.getElementById('balance');
+    const balanceText = balance.innerText;
+    balance.innerText = parseFloat(balanceText) + deposit;
+});
+
+//Withdraw function
+document.getElementById('withdraw-submit').addEventListener("click", function() {
+    const withdrawAmount = document.getElementById('withdraw-amount');
+    const withdrawText = withdrawAmount.value;
+    const withdraw = parseFloat(withdrawText);
+
+    //
+    const totaWithdrawAmount = document.getElementById('total-withdraw-amount');
+    const totalWithdrawText = totaWithdrawAmount.innerText;
+    totaWithdrawAmount.innerText = parseFloat(totalWithdrawText) + withdraw;
+
+    //Clear the withdraw input field
+    withdrawAmount.value = "";
+
+    //Update balance with withdraw
+    const balance = document.getElementById('balance');
+    const balanceText = balance.innerText;
+    const previousBalanceTotal = parseFloat(balanceText);
+    balance.innerText =
+        previousBalanceTotal - withdraw;
+    //Clear the withdraw input field
+    withdrawAmount.value = "";
 });
